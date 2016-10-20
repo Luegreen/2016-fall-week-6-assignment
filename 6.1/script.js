1ba848cc49fbd7c8bcd4b10973430e2b7cc9923d
Luegreen <Luegreen@gmail.com>
2016-10-19 23:13:53 -0400

    bars in ascending order (not descending) using join, still no axis, labels

--- 6.1/script.js
+++ 6.1/script.js
@@ -26,12 +26,18 @@ function parse(d){
             count1960: +d['1960'],
             count2012: +d['2012']
         }
+        
 }
+
 
 function dataLoaded(err,rows){
         console.table(rows),
         console.log(rows);
-
+    
+//        rows.slice(0,5);
+//        rows.sort(function(b,a){
+//            return b.count2012 - a.count2012;
+//        })
 
                                                     //Mining for min and max
         var minCount = d3.min(rows, function(d){ 
@@ -52,80 +58,29 @@ function dataLoaded(err,rows){
             scaleY = d3.scaleLinear()
                        .domain([0, 100])
                        .range([h,0]);
+
+       
+        var shortList = rows.slice(0,5);
+        var topDown=shortList.sort(function(b,a){
+            return b.count2012 - a.count2012; 
+        })
+
+        var barWidth = w/topDown.length;
         
-        console.log(scaleX,scaleY);
     
-//        rows.sort(function(b,a){
-//            return b.count2012 - a.count2012;
-//        })
-//    
-//
-//        
-//        rows.slice(0,5);
-
-
-        rows.forEach(function(country, i){
-            plot.append('rect')
-                .attr("x",m.l+ i*100)
-                .attr("y",300-scaleY(country.count2012))
-                .attr("width", 40)
-                .attr("height",scaleY(country.count2012));
-            
-            console.log(scaleY(country.count2012));
-            
-            plot.append('text')
-                .attr('x',i * 100 +m.l +m.r/2)
-                .attr('y',330)
-                .text(country.countryName)
-                .attr('text-anchor','middle');
-            
-        for(var y = 0; y <= h; y += 20){
-            plot.append('line').attr('class','axis')
-            .attr('x1',0)
-            .attr('x2',w*2)
-            .attr('y1',y)
-            .attr('y2',y);
-        }
-        })  
+    var bars = plot.selectAll("rect")   // plot is considered the 'svg container'
+                        .data(topDown)
+                        .enter()
+                        .append("rect")
+                        .attr("x", function (d) { return (d.count2012*5); })
+                        .attr("y", function(d) { return scaleY(d.count2012); })
+                        .attr("height", function(d) { return h - scaleY(d.count2012); })
+                        .attr("width", barWidth - 1);
+    
+//                plot.append("text")
+//                        .attr("x", function (d) { 
+//                                return (d.count2012*5); })
+//                        .attr("y", function(d) { return                           scaleY(d.count2012) + 60; })
+//                        .attr("dy", ".75em"); 
     
 }
-        
-    
-//    rows.forEach(function(country, i){
-//        var bar = plot.selectAll("div");
-//        .data(i)
-//        .enter()
-//        plot.append('div')
-//            .style("width", function(d) { return x(d) + //"px"; })
-//    .text(function(d) { return d; });
-//    });
-//    
-//    
-//    
-//    var bar = plot.selectAll(".bar")
-//    .data(bin.enter().append("g"))
-//    .attr("class", "bar")
-//    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
-//
-//bar.append("rect")
-//    .attr("x", 1)
-//    .attr("w", x(plot[].x1) - x(plot[].x0) - 1)
-//    .attr("h", function(d) { return h - y(d.length); });
-//
-//bar.append("text")
-//    .attr("dy", ".75em")
-//    .attr("y", 6)
-//    .attr("x", (x(plot[].x1) - x(plot[].x0)) / 2)
-//    .attr("text-anchor", "middle")
-//    .text(function(d) { return formatCount(d.length); });
-//
-//plot.append("g")
-//    .attr("class", "axis axis--x")
-//    .attr("transform", "translate(0," + h + ")")
-//    .call(d3.axisBottom(x));
-//
-//    
-//
-//
-//  
-//  } }
