969d1d11411e1894e029a9b022165c416043f99b
Luegreen <Luegreen@gmail.com>
2016-10-24 21:01:18 -0400

    bars in correct order!! labels, white ticks; need to center labels and add space between axis and first bar

--- 6.1/script.js
+++ 6.1/script.js
@@ -34,10 +34,6 @@ function dataLoaded(err,rows){
         console.table(rows),
         console.log(rows);
     
-//        rows.slice(0,5);
-//        rows.sort(function(b,a){
-//            return b.count2012 - a.count2012;
-//        })
 
                                                     //Mining for min and max
         var minCount = d3.min(rows, function(d){ 
@@ -48,39 +44,54 @@ function dataLoaded(err,rows){
             return d.count2012; 
             }); 
     
-        console.log(minCount,maxCount);
-   
+
     
-                                                    //Scaling x and y axis   
+console.log(minCount,maxCount);
+   var topDown = rows.sort(function(a,b){
+            return b.count2012 - a.count2012;
+        });
+    var shortList = topDown.slice(0,5);
+    
+    
+        //Scaling x and y axis   
         var scaleX = d3.scaleLinear()
                        .domain([minCount,maxCount])
                        .range([0,w]);
             scaleY = d3.scaleLinear()
-                       .domain([0, 100])
+                       .domain([0, 120])
                        .range([h,0]);
+    
+    console.log("domain", shortList.map(function(d){ return d.country; }));
+    console.log("range", d3.range(0, w, w/shortList.length));
+            countryScale = d3.scaleOrdinal()
+                       .domain(shortList.map(function(d){ return d.country; }))
+                       .range(d3.range(0, w, w/shortList.length ));
 
        
-        var shortList = rows.slice(0,5);
-        var topDown=shortList.sort(function(b,a){
-            return b.count2012 - a.count2012; 
-        })
 
-        var barWidth = w/topDown.length;
-        
-    
-    var bars = plot.selectAll("rect")   // plot is considered the 'svg container'
-                        .data(topDown)
+        var bars = plot.selectAll("rows")   // plot is considered the 'svg container'
+                        .data(shortList)
                         .enter()
                         .append("rect")
-                        .attr("x", function (d) { return (d.count2012*5); })
+                        .attr("x", function (d) { return countryScale(d.country); })
                         .attr("y", function(d) { return scaleY(d.count2012); })
-                        .attr("height", function(d) { return h - scaleY(d.count2012); })
-                        .attr("width", barWidth - 1);
+                        .attr("height", function(d) { return h-scaleY(d.count2012); })
+                        .attr("width", 30);
     
-//                plot.append("text")
-//                        .attr("x", function (d) { 
-//                                return (d.count2012*5); })
-//                        .attr("y", function(d) { return                           scaleY(d.count2012) + 60; })
-//                        .attr("dy", ".75em"); 
     
+        bars.append('text')
+                        .text(function(d){return d.code});
+
+    //Represent: axis
+        var axisX = d3.axisBottom()
+                        .scale(countryScale )
+                        .tickSize(-h);
+        var axisY = d3.axisLeft()
+                        .scale(scaleY)
+                        .tickSize(-w);
+        plot.append('g').attr('class','axis axis-x')
+                        .attr('transform','translate(0,'+h+')')
+                        .call(axisX);
+        plot.append('g').attr('class','axis axis-y').call(axisY);
 }
+
